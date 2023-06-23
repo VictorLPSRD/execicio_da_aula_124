0906425b183f4616164a4ea94cb095446a043ba4
Victor Lopes <125214784+VictorLPSRD@users.noreply.github.com>
2023-06-23 01:14:30 -0300

    Update execicio_124.py

--- execicio_124.py
+++ execicio_124.py
@@ -16,36 +16,36 @@ perguntas = [
     },
 ]
 
-qtd_acertos = 0
-for pergunta in perguntas:
-    print('Pergunta:', pergunta['Pergunta'])
+qtd_acertos = 0 # A quantidade de perguntas começa com zero.
+for pergunta in perguntas: # pegando todas as perguntas do dicionário.
+    print('Pergunta:', pergunta['Pergunta']) # exibindo todos as perguntas do dicionário.
+    print()# spaços entre as perguntas 
+
+    opcoes = pergunta['Opções'] 
+    for i, opcao in enumerate(opcoes): # pegando todas as resposta e enumerando ela (i).
+        print(f'{i})', opcao) 
     print()
 
-    opcoes = pergunta['Opções']
-    for i, opcao in enumerate(opcoes):
-        print(f'{i})', opcao)
-    print()
+    escolha = input('Escolha uma opção: ') # pegando a resposta do usuario  ele sempre vai para no input .
 
-    escolha = input('Escolha uma opção: ')
+    acertou = False # O acerto começa com False .
+    escolha_int = None #  A escolha começa como None.
+    qtd_opcoes = len(opcoes) # Pegando a quantidade de opçoes por pergunta .
 
-    acertou = False
-    escolha_int = None
-    qtd_opcoes = len(opcoes)
+    if escolha.isdigit(): # O isdigiti serve para verificar se oq o usuario colocou é um digito  .
+        escolha_int = int(escolha) # Se oq o usuario digitou não for um digito ele vai ser convertido .
 
-    if escolha.isdigit():
-        escolha_int = int(escolha)
-
-    if escolha_int is not None:
-        if escolha_int >= 0 and escolha_int < qtd_opcoes:
-            if opcoes[escolha_int] == pergunta['Resposta']:
-                acertou = True
+    if escolha_int is not None: #  Se a escolha do usuario não for None ele cair aq.
+        if escolha_int >= 0 and escolha_int < qtd_opcoes: # se a escolha_int for maior igual que zero e ecolha_int for meno que qtd_opocoes 
+            if opcoes[escolha_int] == pergunta['Resposta']:# Se opçoes com a ecolha_int  for igual a pergunya com a resposta certa .
+                acertou = True # A escolha que era False vai se torna True.
 
     print()
-    if acertou:
-        qtd_acertos += 1
-        print('Acertou')
-    else:
-        print('Errou')
+    if acertou: # Se acertou  
+        qtd_acertos += 1 # vai somar a quantidade de acertos 
+        print('Acertou') # e vai exibir Acertou 
+    else: # se não ele vai qxibir 
+        print('Errou') # isso
 
     print()
 
